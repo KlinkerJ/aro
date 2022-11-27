aaf94059e071a87f98052803d323974114471b3c
lennart2810 <fuhrig.lennart@web.de>
2022-11-27 15:02:50 +0100

    added sonar callback to hector node

--- nodes/hector_node.py
+++ nodes/hector_node.py
@@ -3,6 +3,7 @@
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Range
 
 # vllt argument für remote controll?!
 class HectorNode(object):
@@ -11,9 +12,9 @@ class HectorNode(object):
         rospy.init_node('hecotr_node')
 
         self.odometry = Odometry()
-        rospy.Subscriber("/ground_truth/state", Odometry , self.pose_callback)
+        rospy.Subscriber("/ground_truth/state", Odometry , self.pose_callback) # topic published with 100 Hz
 
-        rospy.Subscriber("/ground_truth/state", Odometry , self.pose_callback)
+        rospy.Subscriber("/sonar_height", Range , self.sonar_callback) # 10 Hz
 
         # cmd vel innerhalb der drohne erstmal raus lassen
         # self.pub_vel = rospy.Publisher('cmd_vel', Twist, queue_size=1)
@@ -22,9 +23,15 @@ class HectorNode(object):
     def pose_callback(self, data):
 
         # save actual drone position (x,y,z [m]) in global Odometry variable
-        self.odometry.pose.pose.position.x = "{0:.2f}".format(data.pose.pose.position.x)
-        self.odometry.pose.pose.position.y = "{0:.2f}".format(data.pose.pose.position.y)
-        self.odometry.pose.pose.position.z = "{0:.2f}".format(data.pose.pose.position.z)
+        self.odometry.pose.pose.position.x = round(data.pose.pose.position.x, 2)
+        self.odometry.pose.pose.position.y = round(data.pose.pose.position.y, 2)
+        self.odometry.pose.pose.position.z = round(data.pose.pose.position.z, 2)
+
+        #self.odometry.pose.pose.position.x = "{0:.2f}".format(data.pose.pose.position.x)
+
+    def sonar_callback(self, data):
+        m  = round(data.range, 2)
+        print(self.odometry.pose.pose.position.x, self.odometry.pose.pose.position.y, m)
 
 
 if __name__ == '__main__':
