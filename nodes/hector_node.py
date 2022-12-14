963ad06fdcde43b3cdcdbf775916897e4ff9b6de
KlinkerJ <jonasklinker@gmail.com>
2022-12-14 15:45:53 +0100

    Added everything to hector node

--- nodes/hector_node.py
+++ nodes/hector_node.py
@@ -7,22 +7,31 @@ from std_msgs.msg import Bool, Empty
 from sensor_msgs.msg import Range
 import numpy as np
 import math
+import db
+import sektoren
 
 # vllt argument für remote controll?!
+
+
 class HectorNode(object):
     def __init__(self):
 
-        self.sonar_offset = 0.17 # m 
-        self.drone_z_offset = 0.28 # m
+        self.sonar_offset = 0.17  # m
+        self.drone_z_offset = 0.28  # m
 
         rospy.init_node('hector_node')
 
         self.odometry = Odometry()
-        rospy.Subscriber("/ground_truth/state", Odometry, self.pose_callback) # topic published with 100 Hz
+        rospy.Subscriber("/ground_truth/state", Odometry,
+                         self.pose_callback)  # topic published with 100 Hz
 
-        rospy.Subscriber("/sonar_height", Range , self.sonar_callback) # 10 Hz
-        
+        rospy.Subscriber("/sonar_height", Range, self.sonar_callback)  # 10 Hz
+
         rospy.Subscriber("/release", Empty, self.release_callback)
+
+        rospy.Subscriber("/button_pressed", Empty,
+                         self.button_pressed_callback)
+        rospy.Subscriber("/measure", Empty, self.start_measure_callback)
 
         self.cmd_publisher = rospy.Publisher("/cmd_vel", Twist)
         #self.pub_vel = rospy.Publisher('cmd_vel', Twist, queue_size=1)
@@ -33,28 +42,64 @@ class HectorNode(object):
         # self.flypoints = []
         # spalten als self.spalten
         # for spalte in self.spalten:
-            # first_fly_point_y = spalten[0] - 5
-            # last_fly_point_y = spalten[-1] + 5
-            # self.flypoints.append([[first_fly_point_x, first_fly_point_y], [last_fly_point_x, last_fly_point_y]])
+        # first_fly_point_y = spalten[0] - 5
+        # last_fly_point_y = spalten[-1] + 5
+        # self.flypoints.append([[first_fly_point_x, first_fly_point_y], [last_fly_point_x, last_fly_point_y]])
 
-        # fake flypoints 
-        self.flypoints = [[0,0,10],[20,0],[20,20],[0,20]]
+        # fake flypoints
+        self.flypoints = [[0, 0, 10], [20, 0], [20, 20], [0, 20]]
 
         # flypoints müssen aus den segmentmittelpunkten erstellt werden
 
+    def button_pressed_callback(self):
+        # initialize array if not existing
+        if self.corners is not []:
+            self.corners = []
+        # add corner to array
+        corner = [self.odometry.pose.pose.position.x,
+                  self.odometry.pose.pose.position.y]
+        self.corners.append(corner)
+        return
 
+    def calulate_segments_callback(self):
+        if not self.corners.length == 4:
+            return
+        segmentsize = 2
+        columns = sektoren.getSektorForEckpunkte(
+            self.corners[0], self.corners[1], self.corners[2], self.corners[3], segmentsize)
+        db.create_segments_in_db(columns)
+        print("Segments created in DB")
+        return
 
-    
+    def start_measure_callback(self):
+        constants = db.get_constants()
+        # get first point
+        margin = 2  # 2m margin, how much southern should the drone start to first segment
+        first_point = db.calculate_first_point(
+            constants.min_x, constants.min_y, constants.max_y, margin)
+
+        finished = False
+        while not finished:
+            # get next point
+            next_point = db.calculate_next_point(
+                constants.min_x, constants.max_x, constants.min_y, constants.max_y, constants.segmentsize, constants.tolerance, margin, self.odometry.pose.pose.position.x, self.odometry.pose.pose.position.y)
+            if not next_point:
+                finished = True
+            # fly to next point
+            # simulate flight
+            print("Flying to next point:", next_point)
+            self.flyToPosition(next_point)
+        return
+
     def release_callback(self, data):
 
         # release drone with empty pub on '/release' - topic
-            
+
         for point in self.flypoints:
             self.flyToPosition(point)
 
-        
-        # hier muss 
-        
+        # hier muss
+
         # for i, spalte in enumerate(self.spalten):
         #     spaltenanfang = self.flypoints[i][0]
         #     spaltenende = self.flypoints[i][1]
@@ -69,57 +114,59 @@ class HectorNode(object):
 
         return
 
-    def flyToPosition(self, point, tol = 0.2, p_x = 0.2, p_y = 0.2, p_z = 0.2):
-        
+    def flyToPosition(self, point, tol=0.2, p_x=0.2, p_y=0.2, p_z=0.2):
+
         # get x and y value [m] from goal position
         x = point[0]
         y = point[1]
-        
+
         # checking for z-value
         if len(point) == 3:
             z = point[2]
         else:
             z = self.odometry.pose.pose.position.z
-        
-        # calculate errors (x,y,z) between goal position and current drone position
-        e_x = x - self.odometry.pose.pose.position.x 
-        e_y = y - self.odometry.pose.pose.position.y 
-        e_z = z - self.odometry.pose.pose.position.z 
-        
 
-        e_x, e_y = self.rotationShift(e_x, e_y, self.odometry.pose.pose.orientation.z)
+        # calculate errors (x,y,z) between goal position and current drone position
+        e_x = x - self.odometry.pose.pose.position.x
+        e_y = y - self.odometry.pose.pose.position.y
+        e_z = z - self.odometry.pose.pose.position.z
+
+        e_x, e_y = self.rotationShift(
+            e_x, e_y, self.odometry.pose.pose.orientation.z)
 
         cmd_vel = Twist()
-        
+
         # p - controller for position "navigation"
         while abs(e_x) > tol or abs(e_y) > tol or abs(e_z > tol):
 
             q_x = e_x * p_x
             q_y = e_y * p_y
             q_z = e_z * p_z
-            
+
             cmd_vel.linear.x = q_x
             cmd_vel.linear.y = q_y
             cmd_vel.linear.z = q_z
-            
+
             self.cmd_publisher.publish(cmd_vel)
 
-            e_x = x - self.odometry.pose.pose.position.x # Error in x
-            e_y = y - self.odometry.pose.pose.position.y # Error in y
-            e_z = z - self.odometry.pose.pose.position.z # Error in z
+            e_x = x - self.odometry.pose.pose.position.x  # Error in x
+            e_y = y - self.odometry.pose.pose.position.y  # Error in y
+            e_z = z - self.odometry.pose.pose.position.z  # Error in z
 
-            e_x, e_y = self.rotationShift(e_x, e_y, self.odometry.pose.pose.orientation.z)
+            e_x, e_y = self.rotationShift(
+                e_x, e_y, self.odometry.pose.pose.orientation.z)
 
         else:
             cmd_vel.linear.x = 0
             cmd_vel.linear.y = 0
-                
+
             self.cmd_publisher.publish(cmd_vel)
 
     # controller for planar rotational shift (x-y-plane, rotation about z)
     def rotationShift(self, x, y, theta):
-        rot = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]]) # Drehmatrix
-        rot = np.linalg.inv(rot) # Inverse Drehmatrix
+        rot = np.array([[np.cos(theta), -np.sin(theta)],
+                       [np.sin(theta), np.cos(theta)]])  # Drehmatrix
+        rot = np.linalg.inv(rot)  # Inverse Drehmatrix
         new_x = (rot[0][0] * x) + (rot[0][1] * y)
         new_y = (rot[1][0] * x) + (rot[1][1] * y)
         return new_x, new_y
@@ -138,18 +185,21 @@ class HectorNode(object):
         #     else:
         #         self.heightsForSpalte[index].append(round(data.range, 2))
 
-
     def pose_callback(self, data):
 
         # save actual drone position (x,y,z [m]) and orientation (z [°]) in global odometry variable
-        self.odometry.pose.pose.position.x = round(data.pose.pose.position.x, 2)
-        self.odometry.pose.pose.position.y = round(data.pose.pose.position.y, 2)
-        self.odometry.pose.pose.position.z = round(data.pose.pose.position.z, 2)
+        self.odometry.pose.pose.position.x = round(
+            data.pose.pose.position.x, 2)
+        self.odometry.pose.pose.position.y = round(
+            data.pose.pose.position.y, 2)
+        self.odometry.pose.pose.position.z = round(
+            data.pose.pose.position.z, 2)
 
-        self.odometry.pose.pose.orientation.z = round(self.quaterionToRads(data), 2)
+        self.odometry.pose.pose.orientation.z = round(
+            self.quaterionToRads(data), 2)
 
     # transform coordinates and angles from quaternion to values in radiant
-    def quaterionToRads(self, data): # aus ui_hector_quad.py
+    def quaterionToRads(self, data):  # aus ui_hector_quad.py
         x = data.pose.pose.orientation.x
         y = data.pose.pose.orientation.y
         z = data.pose.pose.orientation.z
@@ -166,9 +216,9 @@ class HectorNode(object):
     # calculate the current segment based on the drone position
     def calculate_current_segment(self):
         current_y = self.odometry.pose.pose.position.y
-        current_sm = [self.currentX, min(self.currentYs, key=lambda x:abs(x-current_y))]
+        current_sm = [self.currentX, min(
+            self.currentYs, key=lambda x:abs(x-current_y))]
         return current_sm
-
 
 
 if __name__ == '__main__':
