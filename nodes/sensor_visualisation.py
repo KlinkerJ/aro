ce3ecc5637cf4385caa2b57741e724e67a8996a1
MH98 <114980789+MH98@users.noreply.github.com>
2023-01-13 10:39:59 +0100

    some clean up and setting of comments

--- nodes/sensor_visualisation.py
+++ nodes/sensor_visualisation.py
@@ -1,6 +1,4 @@
-import pandas as pd 
 import numpy as np
-import random
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
@@ -12,9 +10,6 @@ import sektoren
 # read the height data from the SQL-database
 database = SqliteDatabase('../aro-1/db/segments.sqlite')
 segments = db.Segments.select().execute()
-
-segments = list(segments)
-print(segments)
 
 # sort the data in the SQL-database: 
 # first: sort by sm_y, second: group by sm_y, third: sort by sm_x
@@ -31,37 +26,36 @@ for a, files in grouper:
     z = []
     id = []
     for i in r:
-        z.append(i.height) #i.height is correct
+        z.append(i.height)
     result.append(z)
-# print(result)
 
+# some preperation for the grid in the visualisation
+# and the axes labels
 x_label = []
 x_value = 1.0
 for i in result[[0][0]]:
     x_label.append(x_value)
     x_value = x_value+1.0
-# print(x_label)
 
 y_label = []
 y_value = 1.0
 for i in result:
     y_label.append(y_value)
     y_value = y_value+1.0
-# print(y_label)
 
-# genereate grid based on the fild size ans colorize the generated grid
-# write the array values into the plot
-# Assumption that the field is rectangular shaped
+# creat plot and set axes labels
 constants = db.get_constants()
 fig, ax = plt.subplots()
 im = ax.imshow(result[::-1], cmap='YlGn')
 ax.set_xticks(np.arange(len(x_label)))
 ax.set_xticklabels(labels=(np.arange(constants['min_x']-constants['segmentsize']/2+0.5, constants['max_x']+constants['segmentsize']/2+0.5)))
-ax.set_yticks(np.arange(len(y_label))) # ! ggf. anpassen
+ax.set_yticks(np.arange(len(y_label)))
 ax.set_yticklabels(labels=(np.arange(constants['min_y']-constants['segmentsize']/2+0.5, constants['max_y']+constants['segmentsize']/2+0.5)))
 ax.set_ylabel('field height'+' '+ str(constants['max_y']-constants['min_y']+constants['segmentsize']) +' '+'m')
 ax.set_xlabel('field width'+' '+ str(constants['max_x']-constants['min_x']+constants['segmentsize']) +' '+'m')
 ax.invert_yaxis()
+
+# genereate grid based on the fild size and colorize the generated grid
 grid_x_value = 0.5
 grid_y_value = 0.5
 
@@ -73,9 +67,11 @@ for i in y_label:
     ax.axhline(grid_y_value, color='k', linewidth=1)
     grid_y_value = grid_y_value+1
 
+# write the array values into the plot
 for (i, j), z in np.ndenumerate(result[::-1]):
     ax.text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
 
+# set plot title and show the plot
 ax.set_title('Height map of the current field')
 cbar = ax.figure.colorbar(im)
 cbar.set_label('growth height'+' '+'[m]', rotation = 270, labelpad=14)
